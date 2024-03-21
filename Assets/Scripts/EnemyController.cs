f2e5c6f83046da5beae3b2e52185c7077ce093f3
OssieFromDK <50819244+OssieFromDK@users.noreply.github.com>
2024-03-21 13:54:37 +0100

    More shit

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
