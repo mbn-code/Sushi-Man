09ccbcf11cb48834badcbf6fc9f52d325955b0f2
1cy50ng3b0b <66748817+mbn-code@users.noreply.github.com>
2024-03-22 09:36:07 +0100

    Fix camera follow and map collision with animations.

--- Assets/Scripts/PlayerCamera.cs
+++ Assets/Scripts/PlayerCamera.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 public class CameraFollow : MonoBehaviour
 {
     public Transform target;
-    public float smoothSpeed = 0.125f;
+    public float smoothSpeed = 0.825f;
     public Vector3 offset;
 
     void LateUpdate()
     {
         if (target != null)
         {
-            Vector3 desiredPosition = target.position + offset;
+            Vector3 desiredPosition = new Vector3(target.position.x + offset.x, target.position.y + offset.y, transform.position.z);
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
             transform.position = smoothedPosition;
 
